903e6a8f71c3d5a17d9d1cbdd5531ca77cb3177b
Cuyler36 <24523422+Cuyler36@users.noreply.github.com>
2024-06-03 14:59:19 -0400

    more work

--- src/static/jaudio_NES/aictrl.c
+++ src/static/jaudio_NES/aictrl.c
@@ -1,9 +1,19 @@
 #include "aictrl.h"
 
 #include "jaudio_NES/memory.h"
+#include "os/OSAlloc.h"
 
 static u16* dac[3];
 static ALHeap audio_hp;
+static BOOL audio_hp_exist = FALSE;
 
-extern void Jac_HeapSetup(void* pHeap, size_t size) {
+extern void Jac_HeapSetup(void* pHeap, s32 size) {
+    if (pHeap != nullptr) {
+        Nas_HeapInit(&audio_hp, (u8*)pHeap, size);
+        audio_hp_exist = TRUE;
+    } else {
+        audio_hp_exist = FALSE;
+    }
 }
+
+extern void* OSAlloc2
